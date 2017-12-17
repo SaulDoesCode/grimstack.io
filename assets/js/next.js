fa092bb37f34b27046a283129d7ef98070d1ea9b
Saul van der Walt <saulvdw@gmail.com>
2017-12-17 13:50:50 +0200

    making progress on /next.html

--- assets/js/next.js
+++ assets/js/next.js
@@ -1,6 +1,16 @@
 {
-  const {dom, domfn: {hasClass, Class, css}, render, each, model, once} = rilti
-  const {aside, div, header, h1} = dom
+  /* global rilti localStorage */
+  const {
+    domfn: {append, hasClass, Class, css},
+    dom,
+    each,
+    model,
+    once,
+    isEmpty,
+    isBool,
+    run
+  } = rilti
+  const {aside, div, header} = dom
 
   var hub = model()
 
@@ -15,25 +25,56 @@
       get rect () { return sidebar.getBoundingClientRect() },
       get toggled () { return hasClass(sidebar, 'toggled') },
       set toggled (state) {
+        css(sidebar, {left: state ? `-${sidebar.rect.width - 5}px` : ''})
         Class(sidebar, 'toggled', state)
         hub.emit.sbToggle(state)
+      },
+      items: new Map(),
+      get activeItem () { return sidebar.activeitem },
+      set activeItem (ai) {
+        if (sidebar.activeitem !== ai && sidebar.items.has(ai)) {
+          sidebar.items.forEach((item, name) => {
+            const isActive = name === ai
+            Class(item, 'active', isActive)
+            if (isActive) {
+              sidebar.activeitem = name
+              hub.emit.sbItem(name, item)
+              if (item.action) item.action(item)
+            }
+          })
+        }
       }
     },
     methods: {
       scaleContentElement (el) {
-        if (window.innerWidth < 760) return css(el, {marginLeft: '', width: ''})
+        if (window.innerWidth < 760) {
+          return css(el, {
+            marginLeft: '',
+            width: ''
+          })
+        }
         const {rect: {width}, toggled} = sidebar
         css(el, {
           marginLeft: toggled ? '4px' : `${width}px`,
           width: toggled ? 'calc(100% - 6px)' : `calc(100% - ${width + 2}px)`
         })
       },
-      item (inner, click = () => {}) {
-        div({
-          render: sidebar,
-          class: 'flexCentered sbItem',
-          on: {click}
-        }, inner)
+      item (inner, action) {
+        sidebar.items.set(
+          inner,
+          div({
+            render: sidebar,
+            class: 'flexCentered sbItem',
+            methods: {action},
+            on: { click () { sidebar.activeItem = inner } }
+          },
+            inner
+          )
+        )
+
+        run(() => {
+          if (!sidebar.activeItem) sidebar.activeItem = inner
+        })
       }
     }
   },
@@ -44,16 +85,29 @@
     fancyHeader('grimstack')
   )
 
-  sidebar.item(
-    `Say something I'm giving up on you`,
-    () => console.log(`Sê iets of ek strip my moer`)
-  )
-
+  hub.views = new Map()
   const main = dom.main({
     render: 'body',
     class: 'contentGrid',
-    methods: { scale () { sidebar.scaleContentElement(main) } },
-    lifecycle: {
+    methods: {
+      scale () { sidebar.scaleContentElement(main) },
+      view (name, show, view) {
+        if (!isBool(show)) [view, show] = [show, false]
+        const viewExists = hub.views.has(name)
+        if (isEmpty(view)) {
+          if (!viewExists) return
+          hub.lastView = hub.activeView
+          hub.activeView = name
+          view = hub.views.get(name)
+        }
+        if (!viewExists) hub.views.set(name, view)
+        if (show) {
+          main.innerHTML = ''
+          return append(main, view)
+        }
+      }
+    },
+    cycle: {
       mount () {
         main.scale()
         const resizer = once.resize(window, () => {
@@ -64,4 +118,31 @@
     }
   })
   hub.on.sbToggle(main.scale)
+
+  const page = (name, {view, action}) => {
+    main.view(name, view)
+    sidebar.item(name, () => {
+      if (action) action(name, view, main)
+      main.view(name, true)
+    })
+    run(() => {
+      if (!hub.activeView) main.view(name, true)
+    })
+  }
+  page.show = name => { sidebar.activeItem = name }
+
+  page(`Poems and Writs`, {
+    view: `This is where all the poems and writs will go`
+  })
+  page(`Projects`, {
+    view: `You will find some of my GitHub Projects and other projects here`
+  })
+  page(`Doodles`, {
+    view: `Little doodles and ideas will be floating around here`
+  })
+
+  const stats = JSON.parse(localStorage.getItem('stats') || '{"views":0}')
+  console.log('testing:', stats)
+  stats.views++
+  localStorage.setItem('stats', JSON.stringify(stats))
 }
